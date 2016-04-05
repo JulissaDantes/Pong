8e04d91db7e78f099cc8b8ed4bf631a51a319b0e
JulissaDantes <julissadcj@gmail.com>
2016-04-04 20:35:43 -0430

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,46 +1,41 @@
-
 var HelloWorldLayer = cc.Layer.extend({
     jugador1:null,    
-    jugador2:null,
-    p1p:null,
-    p2p:null,
-   // bool1: this.collide,
+    jugador2:null,    
     pelota:null,    
     puntuacion1:null,
     puntuacion2:null,
+    speed:null,
+    
+    random: function getRandomInt(min, max) {
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	},
+    
     inicializar:function(){
-        this.p1p = 0;
-        this.p2p = 0;
+        this.speed = 5;
         var size = cc.winSize;
         var color = cc.color(100,100,100);
-        //este sera el jugador
+
         this.jugador1 =  new cc.DrawNode();
         this.jugador1.drawRect(cc.p(0,0),cc.p(20,100),color,3);
         this.jugador1.setPosition(size.width * 0.1,size.height / 2);
         this.addChild(this.jugador1, 1);
-        //Este sera el bot
+
         this.jugador2 =  new cc.DrawNode();
         this.jugador2.drawRect(cc.p(0,0),cc.p(20,100),color,3);
         this.jugador2.setPosition(size.width -(size.width * 0.1),size.height / 2);
-        this.addChild(this.jugador2, 1);  
+        this.addChild(this.jugador2, 1);        
 
         var lineaDivisoria =  new cc.DrawNode();
         lineaDivisoria.drawSegment(cc.p(size.width/2,0),cc.p(size.width/2,size.height),3,color);
         this.addChild(lineaDivisoria,0);
-        //se creacon un tag de -1
+        
         this.pelota =  new cc.DrawNode();
         this.pelota.drawCircle(cc.p(0,0),5,0,100,false,10,color);
-        this.pelota.setPosition(size.width / 2,size.height / 2);
+        this.pelota.setPosition(size.width / 2, size.height / 2);
         this.addChild(this.pelota, 1);
-        if(this.random(0,9) > 5){
-            this.pelota.runAction(cc.sequence(cc.moveBy(60, cc.p(-1090, -150))));
-            
-        }else{
-            this.pelota.runAction(cc.sequence(cc.moveBy(60, cc.p(1967, 200))));
-            
-        }
-        
-        
+        var moveto = cc.moveTo(this.speed, -10, this.random(0, size.height));
+        this.pelota.runAction(moveto);
+
         this.puntuacion1 = new cc.LabelTTF("0","Arial",24);
         this.puntuacion1.setPosition(size.width * 0.4, size.height - (size.height * 0.10));
         this.addChild(this.puntuacion1,0);
@@ -48,132 +43,88 @@ var HelloWorldLayer = cc.Layer.extend({
         this.puntuacion2 = new cc.LabelTTF("0","Arial",24);
         this.puntuacion2.setPosition(size.width - (size.width * 0.4), size.height - (size.height * 0.10));
         this.addChild(this.puntuacion2,0);
-        
     },
-    random: function getRandomInt(min, max) {
-    	return Math.floor(Math.random() * (max - min + 1)) + min;
-	},
-    moveball: function()
-    {
-        //Asignar puntuación NO FUNCIONA
-        if(this.pelota.getPositionX() > this.jugador1.getPositionX())
-        {
-            //Punto para bot
-            this.p2p = this.p2p + 1;
-            this.puntuacion2.setString(this.p2p.toString());
+    
+    mover : function(key, event){
+        var target = event.getCurrentTarget();
+        var size = cc.winSize;
+        switch(key){
+            case 38:
+                target.jugador1.y += 50;
+                break;
+            case 40:
+                target.jugador1.y -= 50;
+                break;
+        }
+        
+        target.jugador2.setPosition(this.jugador2.getPositionX(), this.pelota.getPositionY() - 50);
+    },
+    
+    startPelota : function(){
+        var curScene = cc.director.getRunningScene();
+        var allChildren = curScene.getChildren();
+        var moveto = cc.moveTo(allChildren[0].speed, -10, allChildren[0].random(0, cc.winSize.height));
+        allChildren[0].pelota.stopAllActions();
+        allChildren[0].pelota.runAction(moveto);
+        cc.director.resume();
+    },
+    
+    collision : function(){
+        if( Math.abs(this.pelota.x - this.jugador1.x) <= 10 && Math.abs(this.pelota.y - this.jugador1.y) <= 100){
+            var moveto = cc.moveTo(this.speed, cc.winSize.width+10, this.random(0, cc.winSize.height));
+            this.pelota.stopAllActions();
+            this.pelota.runAction(moveto);
+        }
+        if( Math.abs(this.pelota.x - this.jugador2.x) <= 10 &&  Math.abs(this.pelota.y - this.jugador2.y) <= 100){
+            var moveto = cc.moveTo(this.speed, -10, this.random(0, cc.winSize.height));
+            this.pelota.stopAllActions();
+            this.pelota.runAction(moveto);
+        }
+    },
+    
+    perder : function(){
+        if(this.pelota.x <= cc.winSize.width*0.1 - 10){
+            var num = parseInt(this.puntuacion2.string);
+            num++;
+            this.puntuacion2.string = num;
+            this.pelota.setPosition(cc.winSize.width / 2, cc.winSize.height / 2);
+            cc.director.pause();
+            setTimeout(this.startPelota, 2000);
+//            /Punto para bot
+//            this.p2p = this.p2p + 1;
+//            this.puntuacion2.setString(this.p2p.toString());
             //this.puntuacion2 = cc.LabelTTF(this.p2p.toString(),"Arial",24)
             //this.puntuacion2._originalText = this.p2p.toString();
             //cc.log("puntuacion 2: " + this.puntuacion2._updateString(this.p2p.toString()));
-        }else{
-            //Punto para jugador1
-            this.p1p = this.p1p + 1;
-            this.puntuacion1.setString(this.p1p.toString());
-           // this.puntuacion1 = cc.LabelTTF(this.p1p.toString(),"Arial",24)
+        }
+        if(this.pelota.x >= cc.winSize.width - (cc.winSize.width * 0.1) + 10){
+            var num = parseInt(this.puntuacion1.string);
+            num++;
+            this.puntuacion1.string = num;
+            this.pelota.setPosition(cc.winSize.width / 2, cc.winSize.height / 2);
+            cc.director.pause();
+            setTimeout(this.startPelota, 2000);
+            // this.puntuacion1 = cc.LabelTTF(this.p1p.toString(),"Arial",24)
             //this.puntuacion1._originalText = this.p1p.toString();//this works
             //cc.log("puntuacion 2: " + this.puntuacion1._updateString(this.p1p.toString()));
         }
-        var size = cc.winSize;
-        this.pelota.setPosition(size.width / 2,size.height / 2);
-        if(this.random(0,9) > 5){
-                        this.pelota.runAction(cc.sequence(cc.moveBy(70, cc.p(-1090, -150))));
-
-                    }else{
-                        this.pelota.runAction(cc.sequence(cc.moveBy(70, cc.p(1967, 200))));
-                    }
-        
     },
-    movep2: function()
-    {//ver la position de la bola
-        
-        if(this.pelota.getPositionY() < 300)
-        {
-            if(this.jugador2.getPositionY() <= 500)
-             {
-               this.jugador2.setPosition(this.jugador2.getPositionX(), this.jugador2.getPositionY() + 50);
-                
-             }
-            
-        }else if(this.jugador2.getPositionY() >= 30){
-            this.jugador2.setPosition(this.jugador2.getPositionX(), this.jugador2.getPositionY() - 50);
-        }
-    }
-    ,    
-    movePlayer: function(keyCode, event){
-        var target = event.getCurrentTarget();
-        var size = cc.winSize;
-
-        
-        // Boton Up presionado THIS WORKS
-        if(keyCode == 38)
-        {
-           if(target.jugador1.getPositionY() <= 500)
-             {
-               
-               target.jugador1.setPosition(target.jugador1.getPositionX(), target.jugador1.getPositionY() + 50);
-                 target.movep2();
-                //comprobar limites de pelota
-                if(target.pelota.getPositionX() > 700 || target.pelota.getPositionX() < 100)
-                {
-                    target.moveball();
-                }
-                 cc.log("Player1 X:" + target.jugador1.getPositionX() + " Y:" + target.jugador1.getPositionY());
-
-                 
-              }
-        }
-        
-        // Boton Down presionado THIS WORKS
-        if(keyCode == 40)
-        {
-         if(target.jugador1.getPositionY() >= 30)
-            {
-                target.jugador1.setPosition(target.jugador1.getPositionX(), target.jugador1.getPositionY() - 50);
-                target.movep2();
-                //comprobar limites de la pelota
-                if(target.pelota.getPositionX() > 700 || target.pelota.getPositionX() < 100)
-                {
-                    target.moveball();
-                 //target.inicializar();   Si esto se hace dibuja todo de nuevo  
-                }
-                cc.log("Player1 X:" + target.jugador1.getPositionX() + " Y:" + target.jugador1.getPositionY());
-            }
-        }
-        
-        //choca jugador1
-       /*
-            if(cc.rectIntersectsRect(J1Place, ballPlace))
-            {
-                cc.log("Chocaste");
-                //this.inicializar;
-              
-            }
-       
-            if(cc.rectIntersectsRect(J2Place, ballPlace))
-            {
-                cc.log("Choco");
-                //this.inicializar;
-              
-            }
-       */
-    },
-//    collide:function() 
-//    {
-//        var b = cc.rectIntersectsRect(this.jugador1.getBoundingBox(), this.pelota.getBoundingBox());
-//        return b;
-//    },
+    
     ctor:function () {
         this._super();
         this.inicializar();
-        //aqui escucha el teclado...:D
-        cc.eventManager.addListener
-        (
-            {
-			event: cc.EventListener.KEYBOARD,
-			onKeyPressed:  this.movePlayer//bola: this.pelota.getBoundingBox()
-		    }, 
-        this);
-
+        
+        cc.eventManager.addListener(
+        {
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: this.mover
+        }, this);
+        
+        this.schedule(this.collision, 1/60);
+        this.schedule(this.perder, 1/60);
+        
         return true;
+
     }
 });
 
@@ -184,4 +135,3 @@ var HelloWorldScene = cc.Scene.extend({
         this.addChild(layer);
     }
 });
-
